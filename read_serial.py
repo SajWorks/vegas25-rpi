361ddd40259e0171d3bbdedf7d298d0441ed3851
Matt Boyd <mboyd1993@gmail.com>
2025-07-10 00:25:22 -0500

    Created shared instance of StateManager.  Implemented function to write guesses to Pixels Modulino.  Added logic to update state for player 1 response.

--- read_serial.py
+++ read_serial.py
@@ -2,12 +2,19 @@
 import serial
 import time
 import threading
+from state_manager import state_manager
 
 # Shared dictionary to hold latest values
 latest_data = {"tempF": None, "tempC": None, "humidity": None}
 button1_presses = 0
 button2_presses = 0
 button3_presses = 0
+
+def reset_button_counts():  
+    global button1_presses, button2_presses, button3_presses
+    button1_presses = 0
+    button2_presses = 0
+    button3_presses = 0
 
 def parse_data(line):
     try:
@@ -17,10 +24,13 @@ def parse_data(line):
         button3_pressed = 0
         stripped_line = line.strip()
         if stripped_line == "Button: 1":
+            print("Button 1 pressed")
             button1_pressed = 1
         elif stripped_line == "Button: 2":
+            print("Button 2 pressed")
             button2_pressed = 1
         elif stripped_line == "Button: 3":
+            print("Button 3 pressed")
             button3_pressed = 1
         return button1_pressed, button2_pressed, button3_pressed
     except (IndexError, ValueError):
@@ -37,6 +47,10 @@ def read_serial():
             button1_presses += button1_pressed
             button2_presses += button2_pressed
             button3_presses += button3_pressed
+
+            # If button 3 was pressed, notify state manager
+            if button3_pressed:
+                state_manager.process_button3_press()
     except serial.SerialException as e:
         print(f"Serial error: {e}")
 
